17d5a3f65e50b479d831075015088230e1ceae23
Manthan Ankolekar <manthan.ank@gmail.com>
2023-01-07 10:12:05 +0530

    changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'covid19-tracker-angular-app';
+  title = 'angular-covid19-tracker-app';
 }
